135754fcc3320e4def1b64bebf679efe15480512
Erick <moralerick90@gmail.com>
2024-10-18 09:38:00 -0700

    added console menu

--- mindfulness/Program.cs
+++ mindfulness/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Activity activity = null;
+
+        while (true)
+        {
+            Console.WriteLine("Select an activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    activity = new BreathingActivity();
+                    break;
+                case "2":
+                    activity = new ReflectingActivity();
+                    break;
+                case "3":
+                    activity = new ListingActivity();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice, try again.");
+                    continue;
+            }
+
+            activity.Run();
+        }
+    }
+}
